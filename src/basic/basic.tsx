62dc3b93eecb9f788e9e2a552f40e7544c7adb95
Aurelian Spodarec <aurelian.spod@gmail.com>
2023-03-25 15:23:10 +0000

    Initial commit

---
+++ src/basic/basic.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function BasicIndex() {
+    const [formState, setFormState] = useState({})
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default BasicIndex;
